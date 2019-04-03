523c190c0d745e51978774f012283f02e4bf0258
Br <br@Brs-MacBook-Pro.local>
2019-04-03 00:27:49 -0700

    assignment introduction to express complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marcoPolo = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -12,7 +13,23 @@ describe("routes : static", () => {
                 expect(res.statusCode).toBe(200);
 
                 done();
-            })
-        })
-    })
-})
+            });
+        });
+    });
+
+    describe("GET /", () => {
+        it('should return status code 200', (done) => {
+
+            request.get(marcoPolo, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+        it("should return the string Polo", (done) => {
+            request.get(marcoPolo, (err, res, body) => {
+                expect(body).toBe("Polo");
+                done();
+            });
+        });
+    });
+});
