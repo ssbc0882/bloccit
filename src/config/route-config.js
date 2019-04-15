044222d30f65cd3aa63cc5514494421c9b119150
Br <br@Brs-MacBook-Pro.local>
2019-04-15 15:49:12 -0700

    checkpoint for post resource part 1 complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,9 +2,7 @@ module.exports = {
     init(app) {
         const staticRoutes = require("../routes/static");
         const topicRoutes = require("../routes/topics");
-        const advertisementsRoutes = require("../routes/advertisements")
 
-        app.use(advertisementsRoutes);
         app.use(staticRoutes);
         app.use(topicRoutes);
 
