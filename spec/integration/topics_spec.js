3caf9c429b7cd006a662ee608389465836225401
Br <br@Brs-MacBook-Pro.local>
2019-04-23 16:24:26 -0700

    add associations to User model

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -3,24 +3,50 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/topics/";
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
+const User = require("../../src/db/models").User;
 
 describe("routes : topics", () => {
 
     beforeEach((done) => {
         this.topic;
+        this.post;
+        this.user;
+
         sequelize.sync({ force: true }).then((res) => {
 
-            Topic.create({
-                title: "JS Frameworks",
-                description: "There is a lot of them"
+            User.create({
+                email: "starman@tesla.com",
+                password: "Trekkie4lyfe"
             })
-                .then((topic) => {
-                    this.topic = topic;
-                    done();
-                })
-                .catch((err) => {
-                    console.log(err);
-                    done();
+                .then((user) => {
+                    this.user = user;
+
+
+                    Topic.create({
+                        title: "JS Frameworks",
+                        description: "There is a lot of them",
+
+                        posts: [{
+                            title: "My first visit to Proxima Centauri b",
+                            body: "I saw some rocks",
+                            userId: this.user.id
+                        }]
+                    }, {
+
+                            include: {
+                                model: Post,
+                                as: "posts"
+                            }
+                        })
+                        .then((topic) => {
+                            this.topic = topic;
+                            this.post = topic.posts[0];
+                            done();
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            done();
+                        })
                 })
         })
     })
